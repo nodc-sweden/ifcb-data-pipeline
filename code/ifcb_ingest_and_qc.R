c5691c5feac99607a5bc72326f24aa3990d657ad
ifcb.u <ifcb.u@ad.smhi.se>
2026-06-10 12:45:53 +0200

    rename feature files

--- code/ifcb_ingest_and_qc.R
+++ code/ifcb_ingest_and_qc.R
@@ -444,6 +444,11 @@ if (length(bins_to_process) > 0) {
                         bins = as.list(bins_to_process),
                         parallel = TRUE,
                         n_cores = 3)
+  
+  # Rename files to allow process by IFCB Dashboard
+  old <- dir_ls(feature_folder, recurse = TRUE, regexp = "_features_v\\d+\\.csv$")
+  new <- gsub("_features_v", "_fea_v", old)
+  file_move(old, new)
 }
 
 # -------------------------------
